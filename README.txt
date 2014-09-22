8a1393331d3724b6bdbf389c43ce45441fb27832
Giacomo <Giacomo@192.168.1.104>
2014-09-22 16:52:50 +0200

    version 1.0.2

---
+++ README.txt
@@ -0,0 +1,6 @@
+Run Golden Cows Profit Calculator
+
+For any problems contact me here:
+	goldencows.pc.help@gmail.com
+and here:
+	www.gcpc.altervista.org
---
+++ src/golden/cows/comprofitcalculator/GoldenCowsComProfitCalculator.java
@@ -0,0 +1,40 @@
+/*
+    Copyright 2014 - Giacomo Pinardi
+
+    This file is part of GoldenCows.com Profit Calculator.
+
+    GoldenCows.com Profit Calculator is free software: you can redistribute 
+    it and/or modify it under the terms of the GNU General Public License as 
+    published by the Free Software Foundation, either version 3 of the License,
+    or (at your option) any later version.
+
+    GoldenCows.com Profit Calculator is distributed in the hope that 
+    it will be useful, but WITHOUT ANY WARRANTY; without even the 
+    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+    See the GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GoldenCows.com Profit Calculator. If not, see <http://www.gnu.org/licenses/>.
+
+    Based On a Game of: 2014 LOFTBOX, LLC. ALL RIGHTS RESERVED В® - golden-cows.com
+ */
+
+package golden.cows.comprofitcalculator;
+
+/**
+ *
+ * @author Giacomo
+ */
+public class GoldenCowsComProfitCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        GraphicInterface GUI = new GraphicInterface();
+        
+        GUI.show();
+    }
+    
+}
